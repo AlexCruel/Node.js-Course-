2b8a27b37c883e0a94ae80ab3b7d6184e5e1c2f8
Alex <sasha.karpenkov@outlook.com>
2020-01-14 12:40:28 +0300

    completed the sixth section

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,17 +1,23 @@
-const request = require('request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const url = 'https://api.darksky.net/forecast/c6b962ec80184bf4532a1ac4e64e95a9/37.8267,-122.4233'
+const address = process.argv[2]
 
-request({ url: url, json: true }, (error, response) => {
-    //console.log(response.body.currently)
-    console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. ' +
-        'There is a ' + response.body.currently.precipProbability + '% chance of rain')
-})
+if (!address) {
+    console.log('Please, provide an address')
+} else {
+    geocode(address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return console.log(error)
+        }
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYWxleGNydWVsIiwiYSI6ImNrNTlrYWp0MDB4dm8zZ3BhOGNxcDZvN2EifQ.N3pq2TQIrHlUhPUrrlgynw'
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
 
-request({ url: geocodeURL, json: true }, (error, response) => {
-    const latitude = response.body.features[0].center[1]
-    const longitude = response.body.features[0].center[0]
-    console.log(latitude, longitude)
-})
+            console.log(location)
+            console.log(forecastData)
+        })
+    })
+}
